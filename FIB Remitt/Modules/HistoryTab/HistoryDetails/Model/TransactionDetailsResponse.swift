cd41e29da7e67379cf9e6ca17e1b5717d6f31613
Sabbir <nasir@newroztech.com>
2024-01-28 18:18:47 +0600

    transaction details view model, response, repository

--- FIB Remitt/Modules/HistoryTab/HistoryDetails/Model/TransactionDetailsResponse.swift
+++ FIB Remitt/Modules/HistoryTab/HistoryDetails/Model/TransactionDetailsResponse.swift
@@ -8,111 +8,35 @@
 
 import Foundation
 
-// MARK: - TransactionDetails
-struct TransactionDetailsResponse: Codable, CustomStringConvertible {
-    var description: String{ return "" }
-    let message: String?
-    let errors: [String]?
-    let data: TransactionDetailContent?
-    
-    enum CodingKeys: String, CodingKey {
-        case message = "message"
-        case errors = "errors"
-        case data = "data"
-    }
-    
-    init(message: String?, errors: [String]?, data: TransactionDetailContent?){
-        self.message = message
-        self.errors = errors
-        self.data = data
-    }
-}
-
-// MARK: - DataClass
-struct TransactionDetailContent: Codable, CustomStringConvertible {
+// MARK: - TransactionDetailsResponse
+struct TransactionDetailsResponse: Codable ,CustomStringConvertible {
     var description: String{ return "" }
     
     let transactionNumber: String?
+    let receiver: String?
     let collectionPoint: String?
-    let transaction: Transaction?
-    let receiver: ReceiverData?
-    let sender: String?
+    let purposeTitle: String?
+    let transaction: TransactionData?
     let status: String?
     let createdAt: String?
     let updatedAt: String?
     
     enum CodingKeys: String, CodingKey {
         case transactionNumber = "transactionNumber"
-        case collectionPoint = "collectionPoint"
-        case transaction = "transaction"
         case receiver = "receiver"
-        case sender = "sender"
+        case collectionPoint = "collectionPoint"
+        case purposeTitle = "purposeTitle"
+        case transaction = "transaction"
         case status = "status"
         case createdAt = "createdAt"
         case updatedAt = "updatedAt"
     }
-    
-    init(transactionNumber: String?, collectionPoint: String?, transaction: Transaction?, receiver: ReceiverData?, sender: String?,status: String?, createdAt: String?, updatedAt: String?) {
-        self.transactionNumber = transactionNumber
-        self.collectionPoint = collectionPoint
-        self.transaction = transaction
-        self.receiver = receiver
-        self.sender = sender
-        self.status = status
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-    }
 }
 
-// MARK: - Receiver
-struct ReceiverData: Codable, CustomStringConvertible {
+// MARK: - Transaction Details Data
+struct TransactionData: Codable ,CustomStringConvertible {
     var description: String{ return "" }
-    let bankDetails: BankDetails?
     
-    enum CodingKeys: String, CodingKey {
-        case bankDetails = "bankDetails"
-    }
-    
-    init(bankDetails: BankDetails?){
-        self.bankDetails = bankDetails
-    }
-}
-
-// MARK: - BankDetails
-struct BankDetails: Codable, CustomStringConvertible {
-    var description: String{ return "" }
-    let fullName: String?
-    let accountNumber: String?
-    let bankName: String?
-    let phoneNumber: String?
-    let typeOfBeneficiary: String?
-    let address: String?
-    let relationship: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case fullName = "fullName"
-        case accountNumber = "accountNumber"
-        case bankName = "bankName"
-        case phoneNumber = "phoneNumber"
-        case typeOfBeneficiary = "typeOfBeneficiary"
-        case address = "address"
-        case relationship = "relationship"
-    }
-    
-    init(fullName: String?, accountNumber: String?,bankName: String?,phoneNumber: String?, typeOfBeneficiary: String?,address: String?,relationship: String?){
-        self.fullName = fullName
-        self.accountNumber = accountNumber
-        self.bankName = bankName
-        self.phoneNumber = phoneNumber
-        self.typeOfBeneficiary = typeOfBeneficiary
-        self.address = address
-        self.relationship = relationship
-    }
-}
-
-// MARK: - Transaction
-struct TransactionList: Codable, CustomStringConvertible {
-    var description: String{ return "" }
     let fromCurrency: String?
     let amountToTransfer: Int?
     let toCurrency: String?
@@ -127,14 +51,5 @@ struct TransactionList: Codable, CustomStringConvertible {
         case totalPayable = "totalPayable"
         case charge = "charge"
         case amountReceivable = "amountReceivable"
-    }
-    
-    init(fromCurrency: String?,amountToTransfer: Int?,toCurrency: String?,totalPayable: Int?,charge: Int?,amountReceivable: Double?){
-        self.fromCurrency = fromCurrency
-        self.amountToTransfer = amountToTransfer
-        self.toCurrency = toCurrency
-        self.totalPayable = totalPayable
-        self.charge = charge
-        self.amountReceivable = amountReceivable
     }
 }
---
+++ FIB Remitt/Modules/HistoryTab/HistoryDetails/ViewModel/TransactionDetailsRepository.swift
@@ -0,0 +1,43 @@
+//
+//  TransactionDetailsRepository.swift
+//  FIB Remitt
+//
+//  Created by Sabbir Nasir on 28/1/24.
+//
+
+import Combine
+class TransactionDetailsRepository {
+    private var subscribers = Set<AnyCancellable>()
+    
+    //@Published var loginInfo:SignInResponse?
+  //  let vm = AuthViewModel()
+    func transactionDetailsApi(transactionNumber: String)  {
+        APIManager.shared.getData(endPoint: TransactionDetailsEndpoint.TransactionDetails(transactionNumber: transactionNumber), resultType: TransactionDetailsResponse.self, showLoader: true)
+            .sink { completion in
+                switch completion{
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    if let err = error as? NetworkError{
+                        //self.presenter?.loginDidAttempedWithError(errorMsg: error.localizedDescription,toast: true)
+                    }else{
+                       // self.presenter?.loginDidAttempedWithError(errorMsg: error.localizedDescription,toast: false)
+                    }
+                 
+                case .finished:
+                    print("API Called!")
+                }
+            } receiveValue: { result in
+             
+                if let data = result.transaction{
+                    print(data)
+//                        self.loginInfo = result
+//                        UserSettings.shared.setLoginInfo(loginInfo: data)
+                       // self.presenter?.loginDidAttempedWithSuccess()
+                       // self.vm.successfullyLoggedIn()
+                    }
+
+               
+            }.store(in: &subscribers)
+
+    }
+}
