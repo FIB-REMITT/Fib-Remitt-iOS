487508dac195317e9b5ecf7a7c73009fdd725f99
devb_raihan <fasi@newroztech.com>
2024-01-28 19:09:58 +0600

    details response added

--- FIB Remitt/Modules/HistoryTab/HistoryDetails/Model/TransactionDetailsResponse.swift
+++ FIB Remitt/Modules/HistoryTab/HistoryDetails/Model/TransactionDetailsResponse.swift
@@ -6,21 +6,20 @@
 //
 
 
+
 import Foundation
 
 // MARK: - TransactionDetailsResponse
-struct TransactionDetailsResponse: Codable ,CustomStringConvertible {
-    var description: String{ return "" }
-    
+struct TransactionDetailsResponse: Codable {
     let transactionNumber: String?
-    let receiver: String?
+    let receiver: DetailsReceiver?
     let collectionPoint: String?
     let purposeTitle: String?
-    let transaction: TransactionData?
+    let transaction: DetailsTransaction?
     let status: String?
     let createdAt: String?
     let updatedAt: String?
-    
+
     enum CodingKeys: String, CodingKey {
         case transactionNumber = "transactionNumber"
         case receiver = "receiver"
@@ -33,17 +32,40 @@ struct TransactionDetailsResponse: Codable ,CustomStringConvertible {
     }
 }
 
-// MARK: - Transaction Details Data
-struct TransactionData: Codable ,CustomStringConvertible {
-    var description: String{ return "" }
-    
+// MARK: - Receiver
+struct DetailsReceiver: Codable {
+    let fullName: String?
+    let phoneNumber: String?
+    let nationality: String?
+    let address: String?
+    let gender: String?
+    let typeOfBeneficiary: String?
+    let relationship: String?
+    let accountNumber: String?
+    let bankName: String?
+
+    enum CodingKeys: String, CodingKey {
+        case fullName = "fullName"
+        case phoneNumber = "phoneNumber"
+        case nationality = "nationality"
+        case address = "address"
+        case gender = "gender"
+        case typeOfBeneficiary = "typeOfBeneficiary"
+        case relationship = "relationship"
+        case accountNumber = "accountNumber"
+        case bankName = "bankName"
+    }
+}
+
+// MARK: - Transaction
+struct DetailsTransaction: Codable {
     let fromCurrency: String?
     let amountToTransfer: Int?
     let toCurrency: String?
     let totalPayable: Int?
     let charge: Int?
     let amountReceivable: Double?
-    
+
     enum CodingKeys: String, CodingKey {
         case fromCurrency = "fromCurrency"
         case amountToTransfer = "amountToTransfer"
