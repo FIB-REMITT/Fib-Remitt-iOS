2f0f720190d1702c5a31593445b951258e34a7f7
Sabbir <nasir@newroztech.com>
2024-01-29 12:13:48 +0600

    added scrollview and created progress component(not dynamic yet)

--- FIB Remitt/Modules/HistoryTab/HistoryDetails/View/HistoryDetailView.swift
+++ FIB Remitt/Modules/HistoryTab/HistoryDetails/View/HistoryDetailView.swift
@@ -14,103 +14,106 @@ struct HistoryDetailView: View {
     @State var traSummary : Bool = false
     
     var body: some View {
-        VStack(spacing: 12){
-            navigationBar
-            SimpleHInfoCellView(title: "Delivery Method", info: "Bank Transfer")
-            SimpleHInfoCellView(title: "Purpose", info:  "")
-            
-            FRVContainer (spacing: 12,backgroundColor: .fr_forground){
-                HStack{
-                    TextBaseMedium(text: "Beneficiary Details", fg_color: .text_Mute)
-                    Spacer()
-                    Image(systemName: benDetails ? "chevron.up" : "chevron.down")
-                }
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    withAnimation{
-                        benDetails.toggle()
+        ScrollView {
+            VStack(spacing: 12){
+                navigationBar
+                SimpleHInfoCellView(title: "Delivery Method", info: "Bank Transfer")
+                SimpleHInfoCellView(title: "Purpose", info:  "Family Support")
+                
+                FRVContainer (spacing: 12,backgroundColor: .fr_forground){
+                    HStack{
+                        TextBaseMedium(text: "Beneficiary Details", fg_color: .text_Mute)
+                        Spacer()
+                        Image(systemName: benDetails ? "chevron.up" : "chevron.down")
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        withAnimation{
+                            benDetails.toggle()
+                        }
+                    }
+                    
+                    if benDetails{
+                        VStack(spacing: 12){
+                            
+                            SimpleHColonInfoView(title: "Name", info:  "")
+                            SimpleHColonInfoView(title: "Bank Name", info: "Ziraat Bank")
+                            SimpleHColonInfoView(title: "Account No.", info: "124 458 458 856")
+                            SimpleHColonInfoView(title: "Ref No.", info: "02748869")
+                            SimpleHColonInfoView(title: "Type", info: "Personal")
+                            SimpleHColonInfoView(title: "Relation", info: "Uncle")
+                            SimpleHColonInfoView(title: "Phone", info: "+90 212 555 1212")
+                            SimpleHColonInfoView(title: "Address", info: "Ankara, Turkey")
+                        }
                     }
                 }
                 
-                if benDetails{
-                    VStack(spacing: 12){
-                        
-                        SimpleHColonInfoView(title: "Name", info:  "")
-                        SimpleHColonInfoView(title: "Bank Name", info: "Ziraat Bank")
-                        SimpleHColonInfoView(title: "Account No.", info: "124 458 458 856")
-                        SimpleHColonInfoView(title: "Ref No.", info: "02748869")
-                        SimpleHColonInfoView(title: "Type", info: "Personal")
-                        SimpleHColonInfoView(title: "Relation", info: "Uncle")
-                        SimpleHColonInfoView(title: "Phone", info: "+90 212 555 1212")
-                        SimpleHColonInfoView(title: "Address", info: "Ankara, Turkey")
+                FRVContainer (spacing: 12,backgroundColor: .fr_forground){
+                    HStack{
+                        TextBaseMedium(text: "Transfer Summary", fg_color: .text_Mute)
+                        Spacer()
+                        Image(systemName: traSummary ? "chevron.up" : "chevron.down")
                     }
-                }
-            }
-            
-            FRVContainer (spacing: 12,backgroundColor: .fr_forground){
-                HStack{
-                    TextBaseMedium(text: "Transfer Summary", fg_color: .text_Mute)
-                    Spacer()
-                    Image(systemName: traSummary ? "chevron.up" : "chevron.down")
-                }
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    withAnimation{
-                        traSummary.toggle()
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        withAnimation{
+                            traSummary.toggle()
+                        }
+                    }
+                    
+                    if traSummary{
+                        VStack(alignment: .leading,spacing: 12){
+                            Text("10/1/2023 | 10:00 PM")
+                                .foregroundColor(Color.text_Mute)
+                                .font(UI.FRAppDesignedFont(style: .smallRegular))
+                            
+                            SimpleHInfoRegularView(title: "Amount to transfer", info: "125,000 IQD")
+                            SimpleHInfoRegularView(title: "Service charge", info: "+ 5,000 IQD")
+                            Divider()
+                            SimpleHModInfoView(title: "Total payble", info: "130,000 IQD",fontStyle: .titleBold)
+                            SimpleHModInfoView(title: "Recipient gets", info: "2,875 TRY", textColor: Color.primary500, fontStyle: .allBold)
+                        }
                     }
                 }
                 
-                if traSummary{
-                    VStack(alignment: .leading,spacing: 12){
-                        Text("10/1/2023 | 10:00 PM")
-                            .foregroundColor(Color.text_Mute)
-                            .font(UI.FRAppDesignedFont(style: .smallRegular))
-                        
-                        SimpleHInfoRegularView(title: "Amount to transfer", info: "125,000 IQD")
-                        SimpleHInfoRegularView(title: "Service charge", info: "+ 5,000 IQD")
-                        Divider()
-                        SimpleHModInfoView(title: "Total payble", info: "130,000 IQD",fontStyle: .titleBold)
-                        SimpleHModInfoView(title: "Recipient gets", info: "2,875 TRY", textColor: Color.primary500, fontStyle: .allBold)
+                FRVContainer (spacing: 12,backgroundColor: .fr_forground){
+                    VStack(alignment: .center){
+                        HStack {
+                            TextBaseMedium(text: "Progress", fg_color: .text_Mute)
+                            Spacer()
+                        }
+                        ZStack{
+                            HStack{
+                                Divider()
+                                    .frame(width: 1)
+                                    .frame(maxHeight: 240)
+                                    .overlay(Color.gray.opacity(0.5))
+                            }
+                            VStack(spacing: 25){
+                                ProgressLeftComponent(status: "Pending", timeDat: "9th March 2023 | 10:00 PM", iconName: "pending", cellColor: Color.warning_regular)
+                                ProgressRightComponent(status: "Hold", timeDat: "9th March 2023 | 10:00 PM", iconName: "pause", cellColor: Color.red)
+                                
+                                ProgressLeftComponent(status: "Approved", timeDat: "9th March 2023 | 10:00 PM", iconName: "approved", cellColor: Color.green)
+                                
+                                ProgressRightComponent(status: "Canceled", timeDat: "9th March 2023 | 10:00 PM", iconName: "close", cellColor: Color.red)
+                                
+                                ProgressLeftComponent(status: "Paid", timeDat: "9th March 2023 | 10:00 PM", iconName: "paid", cellColor: Color.green)
+                            }
+                            .frame(width: 275)
+                        }
+                        .frame(minHeight: 280, maxHeight:200)
                     }
+                    
                 }
-            }
-            
-            //            FRVContainer (spacing: 12,alignment:.leading,backgroundColor: .fr_forground){
-            //                TextBaseMedium(text: "Progress", fg_color: .text_Mute)
-            //
-            //                HStack(spacing: 6){
-            //                    VStack(alignment: .trailing, spacing: 5){
-            //                        Text("Pending")
-            //                        .foregroundColor(Color.warning_regular)
-            //                        .font(UI.FRAppDesignedFont(style: .smallRegular))
-            //                        .padding(.horizontal, 7)
-            //                        .padding(.vertical, 1)
-            //                        .background(Color.warning_regular.opacity(0.1))
-            //                        .overlay(RoundedRectangle(cornerRadius: 100).stroke(Color.warning_regular, lineWidth: 1))
-            //
-            //                        //9th March 2023 | 10:00 PM
-            //
-            //                        Text("9th March 2023 | 10:00 PM")
-            //                        .foregroundColor(Color.text_Mute)
-            //                        .font(UI.FRAppDesignedFont(style: .smallRegular))
-            //                    }
-            //
-            //                    Image("pending")
-            //                        .imageDefaultStyle()
-            //                        .frame(width:16, height: 16)
-            //                        .padding(4)
-            //                        .background(Color.warning_regular.opacity(0.1))
-            //                        .cornerRadius(100)
-            //
-            //                }
-            //            }
-            Spacer()
-        }.padding()
-            .background(Color.fr_background.ignoresSafeArea())
-            .navigationBarHidden(true)
-            .onAppear(perform: {
-                vm.transactionDetailsFetch(transactionNumber: id)
+                
+                Spacer()
+            }.padding()
+                .navigationBarHidden(true)
+                .onAppear(perform: {
+                    vm.transactionDetailsFetch(transactionNumber: id)
             })
+        }
+        .background(Color.fr_background.ignoresSafeArea())
     }
 }
 //MARK: - VIEW COMPONENTS
@@ -127,4 +130,71 @@ extension HistoryDetailView{
 }
 #Preview {
     HistoryDetailView(id: "")
+}
+
+struct ProgressLeftComponent: View {
+    var status: String
+    var timeDat: String
+    var iconName: String
+    var cellColor = Color.warning_regular
+    var alignmentStyle: HorizontalAlignment = .leading
+    var body: some View {
+        HStack(spacing: 6){
+            VStack(alignment: .trailing, spacing: 5){
+                Text(status)
+                    .foregroundColor(cellColor)
+                    .font(UI.FRAppDesignedFont(style: .smallRegular))
+                    .padding(.horizontal, 7)
+                    .padding(.vertical, 1)
+                    .background(cellColor.opacity(0.1))
+                    .overlay(RoundedRectangle(cornerRadius: 100).stroke(cellColor, lineWidth: 1))
+                
+                Text(timeDat)
+                    .foregroundColor(Color.text_Mute)
+                    .font(UI.FRAppDesignedFont(style: .smallRegular))
+            }
+            Image(iconName)
+                .imageDefaultStyle()
+                .frame(width:16, height: 16)
+                .padding(4)
+                .background(cellColor.opacity(0.1))
+                .background(Color.white)
+                .cornerRadius(100)
+            Spacer()
+        }
+    }
+}
+
+struct ProgressRightComponent: View {
+    var status: String
+    var timeDat: String
+    var iconName: String
+    var cellColor = Color.warning_regular
+    var alignmentStyle: HorizontalAlignment = .leading
+    var body: some View {
+        HStack(spacing: 6){
+            Spacer()
+            Image(iconName)
+                .imageDefaultStyle()
+                .frame(width:16, height: 16)
+                .padding(4)
+                .background(cellColor.opacity(0.1))
+                .background(Color.white)
+                .cornerRadius(100)
+            VStack(alignment: .leading, spacing: 5){
+                Text(status)
+                    .foregroundColor(cellColor)
+                    .font(UI.FRAppDesignedFont(style: .smallRegular))
+                    .padding(.horizontal, 7)
+                    .padding(.vertical, 1)
+                    .background(cellColor.opacity(0.1))
+                    .overlay(RoundedRectangle(cornerRadius: 100).stroke(cellColor, lineWidth: 1))
+                
+                Text(timeDat)
+                    .foregroundColor(Color.text_Mute)
+                    .font(UI.FRAppDesignedFont(style: .smallRegular))
+            }
+            
+        }
+    }
 }
