41b5966eeddf09600a316716d1bdd0c452f210a6
devb_raihan <fasi@newroztech.com>
2024-01-31 13:28:27 +0600

    pagination working

--- FIB Remitt/Modules/HistoryTab/HistoryLanding/View/HistoryRootView.swift
+++ FIB Remitt/Modules/HistoryTab/HistoryLanding/View/HistoryRootView.swift
@@ -12,6 +12,8 @@ struct HistoryRootView: View {
     @ObservedObject var vm = TransactionHistoryViewModel() // history view model
     //@State var selectedFilterValue: String = "All"
     @State var pageNo = 0
+    @State var from = ""
+    @State var to = ""
     var body: some View {
         VStack{
             navigationBar
@@ -25,6 +27,8 @@ struct HistoryRootView: View {
         .navigationDestination(isPresented: $vm.goToNext) {vm.destinationView}
         .onAppear(perform: {
             vm.selectedFilterValue = "All"
+            self.from = ""
+            self.to = ""
             initialDataAll()
         })
     }
@@ -65,6 +69,17 @@ extension HistoryRootView{
                     .onTapGesture {
                         vm.navigateToTransactionDetails(transactionNumber: transactionData.transactionNumber ?? "")
                     }
+                    .onAppear {
+                        if let currentIndex = vm.transactionHistoryDataOnly.firstIndex(where: { $0.id == transactionData.id }),
+                           currentIndex == vm.transactionHistoryDataOnly.count - 1 {
+                            if vm.transactionHistoryResponse?.last == false{
+                                pageNo = pageNo + 1
+                                transactionListApi(from: self.from, to: self.to)
+                              
+                            }
+                           
+                        }
+                    }
             }
         }
     }
@@ -82,18 +97,28 @@ extension HistoryRootView{
     
     func filterTheData(){
         if vm.selectedFilterValue == "All"{
+            self.from = ""
+            self.to = ""
             initialDataAll() //from:String = "",to:String = ""
         }else if vm.selectedFilterValue == "Today" {
             let dateRangeToday = vm.getDateRange(for: .today)
+            self.from = dateRangeToday.from
+            self.to = dateRangeToday.to
             initialDataAll(from:"\(dateRangeToday.from)",to: "\(dateRangeToday.to)")
         } else if vm.selectedFilterValue == "Last 7 Days" {
             let dateRangeLast7Days = vm.getDateRange(for: .last7Days)
+            self.from = dateRangeLast7Days.from
+            self.to = dateRangeLast7Days.to
             initialDataAll(from:"\(dateRangeLast7Days.from)",to: "\(dateRangeLast7Days.to)")
         } else if vm.selectedFilterValue == "This Month" {
             let currentMonthRange = vm.getDateRange(for: .currentMonth)
+            self.from = currentMonthRange.from
+            self.to = currentMonthRange.to
             initialDataAll(from:"\(currentMonthRange.from)",to: "\(currentMonthRange.to)")
         } else {
             let currentYearRange = vm.getDateRange(for: .currentYear)
+            self.from = currentYearRange.from
+            self.to = currentYearRange.to
             initialDataAll(from:"\(currentYearRange.from)",to: "\(currentYearRange.to)")
         }
     }
