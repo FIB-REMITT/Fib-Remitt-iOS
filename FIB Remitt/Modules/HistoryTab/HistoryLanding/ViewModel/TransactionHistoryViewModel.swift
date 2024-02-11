73dbfd0f7a1af61fca52d2687d150d90b3033cdb
Ainul <ainul@newroztech.com>
2024-02-11 15:15:30 +0600

    data changes for filter

--- FIB Remitt/Modules/HistoryTab/HistoryLanding/ViewModel/TransactionHistoryViewModel.swift
+++ FIB Remitt/Modules/HistoryTab/HistoryLanding/ViewModel/TransactionHistoryViewModel.swift
@@ -5,27 +5,28 @@ import Combine
 
 
 class TransactionHistoryViewModel : ObservableObject{
+    
+    private var subscribers        = Set<AnyCancellable>()
+
     @Published var goToNext        = false
     @Published var destinationView = AnyView(Text("Destination"))
     
-    @Published var transactionHistoryResponse : TransactionHistoryResponse?
-    @Published var transactionHistoryDataOnly : [TransactionListContent] = []
-    @Published var transactionDetails         : TransactionDetailsResponse?
-    @Published var selectedFilterValue        : String = "All"
+    @Published var transactionHistoryResponse   : TransactionHistoryResponse?
+    @Published var transactionHistoryDataOnly   : [TransactionListContent]    = []
+    @Published var filterTransactionHistoryData : [TransactionListContent]    = []
+    @Published var transactionDetails           : TransactionDetailsResponse?
+    @Published var selectedFilterValue          : String = "All"
     
+    @Published var shouldFilterApply        = false
     @Published var isMorePerpHistoryLoading = false
-    
-    
-    private var subscribers = Set<AnyCancellable>()
+
     let repo = TransactionListRepository()
-    
-    
+
     func navigateToEditBankBeneficiary() {
         self.destinationView = AnyView(EditBeneficiaryBankView())
         self.goToNext        = true
     }
-    
-    
+
     func navigateToTransactionDetails(transactionNumber:String){
         self.destinationView = AnyView(HistoryDetailView(id: transactionNumber))
         self.goToNext        = true
@@ -36,24 +37,33 @@ class TransactionHistoryViewModel : ObservableObject{
         repo.$transactionDetails
             .sink { [weak self] result in
                 self?.transactionDetails = result
-            }
-            .store(in: &subscribers)
+            }.store(in: &subscribers)
     }
 
-    func transactionListFetch(page:Int, from: String, to : String) {
+    func transactionListFetch(page:Int, from: String = "", to : String = "") {
         repo.transactionListApi(page:page ,from:from, to:to)
         repo.$transactionHistoryList.sink { result in
             print(self.transactionHistoryDataOnly.count)
             if result != nil && result != self.transactionHistoryResponse{
                 self.transactionHistoryResponse = result
-                self.transactionHistoryDataOnly += result?.content ?? []//= (result?.content ?? [])
+                self.transactionHistoryDataOnly += result?.content ?? [] //= (result?.content ?? [])
             }
-
         }.store(in: &subscribers)
         
     }
     
-    
+    func filteredTransactionListFetch(page:Int, from: String = "", to : String = "") {
+        repo.transactionListApi(page:page ,from:from, to:to)
+        repo.$transactionHistoryList.sink { result in
+            print(self.transactionHistoryDataOnly.count)
+            if result != nil && result != self.transactionHistoryResponse{
+                self.transactionHistoryResponse = result
+                self.transactionHistoryDataOnly += result?.content ?? [] //= (result?.content ?? [])
+            }
+        }.store(in: &subscribers)
+        
+    }
+
     //MARK: FOR FILTER
     enum DateRangeType {
         case today
@@ -72,8 +82,7 @@ class TransactionHistoryViewModel : ObservableObject{
             
         case .today:
             return (dateFormatter.string(from: currentDate), dateFormatter.string(from: currentDate))
-            
-            
+ 
         case .last7Days:
             guard let sevenDaysAgo = calendar.date(byAdding: .day, value: -7, to: currentDate) else {
                 fatalError("Failed to calculate the date for 7 days ago.")
